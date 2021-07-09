ecafb60bb3f99d1d5bf9bb33a8a3ed448eae0fbd
lwxTG <863340557@qq.com>
2021-07-09 20:11:54 +0800

    Initial commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,11 @@
+import sun.security.krb5.internal.TGSRep;
+
 /**
  * @author lwx
  * @date 2021/7/8 - 15:57
  */
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello, TG");
+    }
 }
